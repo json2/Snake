f312c673f09fe5ee3fb8c5fb23b6691f988b3dd6
unknown <jabec@MININT-2H0L8DE.northamerica.corp.microsoft.com>
2015-08-22 19:55:14 +0800

    Moved main to Snake.ps1

--- Snake/Snake.psd1
+++ Snake/Snake.psd1
@@ -3,6 +3,6 @@ GUID = '5023be01-dac1-4b9a-811d-cfce9224a890'
 Author = 'Jason Beck'
 ModuleVersion = '1.0.0.0'
 RootModule='Snake.psm1'
-FunctionsToExport='main'
+FunctionsToExport='startGame'
 }
 
--- Snake/Snake.psm1
+++ Snake/Snake.psm1
@@ -327,22 +327,3 @@ function startGame(){
         }
     }
 }
-
-function main(){
-    while(startGame){}
-    Write-Host ""
-    Write-Host ""
-    Write-Host "`t`t`tThanks for playing!" -ForegroundColor Green
-    Write-Host ""
-    Write-Host "This program will end in 3 seconds..." -NoNewline
-    sleep -Seconds 1
-    [System.Console]::SetCursorPosition(25, $global:windowHeight + 3)
-    Write-Host "2" -NoNewline
-    Sleep -Seconds 1
-    [System.Console]::SetCursorPosition(25, $global:windowHeight + 3)
-    Write-Host "1" -NoNewline
-    Sleep -Seconds 1
-    [System.Console]::SetCursorPosition(25, $global:windowHeight + 3)
-    Write-Host "0"
-    [System.Console]::CursorVisible = $true
-}
